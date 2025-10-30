0c9cdc212a4599ba8522c4e5b40a67c43b1b82d4
ifilipis <40601736+ifilipis@users.noreply.github.com>
2025-10-30 00:28:03 +0000

    Fix relative import for node mappings

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """Expose ComfyUI node mappings."""
 
-from nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS  # noqa: F401
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS  # noqa: F401
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
